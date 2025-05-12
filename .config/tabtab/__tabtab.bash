7aec9d875c33b81e2915f7e122b8118fa53dcfc4
Craig Israel <craig@theisraels.net>
2025-05-12 12:51:02 -0500

    adds tabtab config

---
+++ .config/tabtab/__tabtab.bash
@@ -0,0 +1,12 @@
+
+# tabtab source for serverless package
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/serverless.bash ] && . ~/.config/tabtab/serverless.bash || true
+
+# tabtab source for sls package
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/sls.bash ] && . ~/.config/tabtab/sls.bash || true
+
+# tabtab source for slss package
+# uninstall by removing these lines
+[ -f ~/.config/tabtab/slss.bash ] && . ~/.config/tabtab/slss.bash || true
