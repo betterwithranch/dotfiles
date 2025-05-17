94b9a04060c15f8beab3f1b5f70e8573dbbf8a0e
Craig Israel <craig@theisraels.net>
2025-05-17 18:09:36 -0500

    use software update to install command line tools

--- scripts/xcode-select.sh
+++ scripts/xcode-select.sh
@@ -12,10 +12,13 @@ if [ $? -ne 0 ]; then
   echo "Xcode command line tools are not installed. Installing ..."
   sudo xcode-select --install
 
+  echo "Select install from the dialog"
+
   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
   PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')
 
   softwareupdate -i "$PROD" --verbose
+
   if [ $? -ne 0 ]; then
     echo "Error installing command line tools. Exiting ..."
     exit 1
