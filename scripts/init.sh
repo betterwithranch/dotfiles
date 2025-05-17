f6b6e2fab9221bd78217fd5fb43138590ba07e4b
Craig Israel <craig@theisraels.net>
2025-05-17 17:58:07 -0500

    gets scripts from repo when called before checkout

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-echo scriptdir=$SCRIPT_DIR
 
 echo "Setting up touch id for sudo commands"
 sed -e 's/^#auth/auth/' /etc/pam.d/sudo_local.template | sudo tee /etc/pam.d/sudo_local
@@ -9,15 +7,23 @@ if [ $? -ne 0 ]; then
   echo "Error updating sudo_local. Exiting ..."
 fi
 
-/bin/bash -c "sudo echo test"
 # Install Xcode command line tools
-source "$SCRIPT_DIR/xcode-select.sh"
+
+curl https://raw.githubusercontent.com/betterwithranch/dotfiles/main/scripts/xcode-select.sh | bash
 
 # Checkout dotfiles repo
-source "$SCRIPT_DIR/checkout.sh"
+curl https://raw.githubusercontent.com/betterwithranch/dotfiles/main/scripts/checkout.sh | bash
+
+# define config alias locally since the dotfiles
+# aren't installed on the system yet
+function config {
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+SCRIPT_DIR=$(config rev-parse --show-toplevel)/scripts
 
 # Install homebrew
-source "$SCRIPT_DIR/homebrew.sh"
+source "$HOME/scripts/homebrew.sh"
 
 echo
 echo
