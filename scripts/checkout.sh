64fe549770fa737bdc54eb0995590905322f950e
Craig Israel <craig@theisraels.net>
2025-05-17 16:46:14 -0500

    Adds sudo touch id and moves checkout script

---
+++ scripts/checkout.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+echo
+echo
+echo Checking out dotfiles repo
+echo
+echo
+
+if [ -z "$DOTFILES_LOCAL" ]; then
+  rm -rf $HOME/.dotfiles
+  git clone --bare https://github.com/betterwithranch/dotfiles.git $HOME/.dotfiles
+
+  if [ $? -ne 0 ]; then
+    echo "Could not clone repo. Exiting..."
+    exit 1
+  fi
+fi
+
+# define config alias locally since the dotfiles
+# aren't installed on the system yet
+function config {
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+config checkout
+
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles from git@github.com:betterwithranch/dotfiles.git"
+else
+  echo "Moving existing dotfiles to ~/.dotfiles-backup"
+  FILES=$(config checkout 2>&1 | egrep "^\s+" | awk {'print $1'})
+
+  for f in $FILES; do
+    mkdir -p $(dirname $HOME/.dotfiles-backup/$f) &&
+      mv $HOME/$f $HOME/.dotfiles-backup/$f
+  done
+fi
+
+# checkout dotfiles from repo
+config checkout
+config config status.showUntrackedFiles no
+if [ $? = 0 ]; then
+  echo "Checkout completed successfully"
+else
+  echo "Checkout could not be completed"
+  exit 1
+fi
