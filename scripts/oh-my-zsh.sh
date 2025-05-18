84349b325c9b97406487e3085cf5c658dce6c1a7
Craig Israel <craig@theisraels.net>
2025-05-18 11:27:15 -0500

    cleans up .zshrc

--- scripts/oh-my-zsh.sh
+++ scripts/oh-my-zsh.sh
@@ -14,6 +14,12 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     echo "Error installing oh-my-zsh. Exiting ..."
     exit 1
   fi
+
+  config checkout .zshrc && rm ~/.zshrc.pre-oh-my-zsh
+  if [ $? -ne 0 ]; then
+    echo "Error restoring .zshrc file. Exiting ..."
+    exit 1
+  fi
 fi
 
 echo "Oh-my-zsh is installed"
