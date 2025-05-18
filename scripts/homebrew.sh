047781dfd5f52b80661ea5be0497dccdb036b5f9
Craig Israel <craig@theisraels.net>
2025-05-17 19:28:45 -0500

    adds brew bundle

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -14,3 +14,11 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Homebrew is installed"
+
+echo "Installing homebrew packages"
+
+brew bundle --file ~/.Brewfile
+if [ $? -ne 0 ]; then
+  echo "An error occurred installing homebrew packages. Exiting ..."
+  exit 1
+fi
