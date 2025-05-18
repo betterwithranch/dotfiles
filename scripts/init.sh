14a5842d1bc292ae83f0bf7abeeedb8090cbde6e
Craig Israel <craig@theisraels.net>
2025-05-17 23:18:15 -0500

    adds pam-reattach to use touch id in tmux

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,10 +5,10 @@ grep "auth\s*sufficient\s*pam_tid.so" /etc/pam.d/sudo_local
 if [ $? -ne 0 ]; then
   echo "Setting up touch id for sudo commands"
   sed -e 's/^#auth/auth/' /etc/pam.d/sudo_local.template | sudo tee /etc/pam.d/sudo_local
-fi
 
-if [ $? -ne 0 ]; then
-  echo "Error updating sudo_local. Exiting ..."
+  if [ $? -ne 0 ]; then
+    echo "Error updating sudo_local. Exiting ..."
+  fi
 fi
 
 # Install Xcode command line tools
@@ -37,6 +37,20 @@ SCRIPT_DIR=$(config rev-parse --show-toplevel)/scripts
 source "$HOME/scripts/homebrew.sh"
 if [ $? -ne 0 ]; then
   exit 1
+fi
+
+grep "/opt/homebrew/lib/pam/pam_reattach.so" /etc/pam.d/sudo_local
+
+if [ $? -ne 0 ]; then
+  echo "Adding pam-reattach"
+
+  sudo sed -i '' '/auth[[:space:]]*sufficient[[:space:]]*pam_tid.so/i\
+    auth     optional     /opt/homebrew/lib/pam/pam_reattach.so\
+    ' /etc/pam.d/sudo_local
+
+  if [ $? -ne 0 ]; then
+    echo "Error updating pam-reattach"
+  fi
 fi
 
 # Restore temporary git config changes
