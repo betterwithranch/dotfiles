7783b6b6b8c6ec84ce7ed7edbc918071f161b890
Craig Israel <craig@theisraels.net>
2025-06-17 11:55:14 -0400

    adds claude

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -1,10 +1,38 @@
 return {
   {
     "olimorris/codecompanion.nvim",
-    opts = {},
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "anthropic",
+        },
+        inline = {
+          adapter = "anthropic",
+        },
+        agent = {
+          adapter = "anthropic",
+        },
+      },
+      adapters = {
+        anthropic = function()
+          return require("codecompanion.adapters").extend("anthropic", {
+            env = {
+              api_key = "cmd:op read op://Employee/Anthropic/apikey --no-newline",
+            },
+            schema = {
+              model = {
+                default = "claude-3-5-sonnet-20241022",
+              },
+            },
+          })
+        end,
+      },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      "hrsh7th/nvim-cmp",
     },
+    config = true,
   },
 }
