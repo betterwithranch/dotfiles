c69062ffe40b29f1b5c6932ab50564f70bcd7928
Craig Israel <craig@theisraels.net>
2026-01-17 08:31:19 -0600

    adds gh, auto-session, render-markdown, worktree, and updates packages

---
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -0,0 +1,10 @@
+return {
+  "rmagatti/auto-session",
+  config = function()
+    require("auto-session").setup({
+      auto_save_enabled = true,
+      auto_restore_enabled = true,
+      suppressed_dirs = { "~/", "~/Downloads", "/" },
+    })
+  end,
+}
---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        enabled = false,
+        auto_trigger = false,
+        hide_during_completion = false, -- This hides cmp when copilot is active
+      },
+    },
+  },
+}
