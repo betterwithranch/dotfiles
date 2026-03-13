250a3b04b8dc4dea15a40d6fcbf22f07cd4549f4
Craig Israel <craig@theisraels.net>
2026-03-12 23:08:12 -0700

    adds favorites for finder sidebar

--- scripts/finder-favorites.sh
+++ scripts/finder-favorites.sh
@@ -9,19 +9,11 @@ defaults write com.apple.sidebarlists favoritesitems -dict-add ShowDesktop -bool
 # Ensure the Sidebar itself is not hidden
 defaults write com.apple.finder ShowSidebar -bool true
 
-# Restart Finder to apply visibility changes
-killall Finder
+# Ensure directories exist
+mkdir -p "$HOME/dev"
 
-# Create sidebar anchor directory for symlinks
-mkdir -p "$HOME/.sidebar_anchors"
+# Register favorites
+/usr/bin/sfltool add-item com.apple.LSSharedFileList.FavoriteItems "file://$HOME/obsidian"
+/usr/bin/sfltool add-item com.apple.LSSharedFileList.FavoriteItems "file://$HOME/dev"
 
-# Create symlinks (work even if targets don't exist yet, e.g. before iCloud login)
-ln -sfn "$HOME/Library/Mobile Documents/iCloud~md~obsidian/Documents" "$HOME/.sidebar_anchors/Obsidian"
-ln -sfn "$HOME/dev" "$HOME/.sidebar_anchors/dev"
-
-# Register anchors with Finder favorites
-/usr/bin/sfltool add-item com.apple.LSSharedFileList.FavoriteItems "file://$HOME/.sidebar_anchors/Obsidian"
-/usr/bin/sfltool add-item com.apple.LSSharedFileList.FavoriteItems "file://$HOME/.sidebar_anchors/dev"
-
-# Restart Finder to pick up new favorites
 killall Finder
